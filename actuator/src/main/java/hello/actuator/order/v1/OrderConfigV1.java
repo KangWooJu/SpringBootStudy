f34139e50a3e4d0a834b913e53b56280f7eb1657
woojuice1102 <122527068+KangWooJu@users.noreply.github.com>
2024-10-08 23:54:26 +0900

    Add files via upload

---
+++ actuator/src/main/java/hello/actuator/order/v1/OrderConfigV1.java
@@ -0,0 +1,15 @@
+package hello.actuator.order.v1;
+
+import hello.order.OrderService;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderConfigV1 {
+
+    @Bean
+    OrderService orderService(MeterRegistry registry) {
+        return new hello.order.v1.OrderServiceV1(registry);
+    }
+}
